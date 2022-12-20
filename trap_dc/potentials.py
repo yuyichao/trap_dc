9ef0009e1c09c5044f6c7ef9aa0eea1b7b839611
Yichao Yu <yyc1992@gmail.com>
2022-12-20 13:45:07 -0500

    Raw potential

--- trap_dc/potentials.py
+++ trap_dc/potentials.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import numpy as np
+import struct
+
 ##
 # Electrode names for Phoenix and Peregrine
 _raw_electrode_names_px = ["GND", "RF"]
@@ -102,3 +105,100 @@ def _alias_to_names(_aliases, trap):
         assert new_id != -1
         electrode_names[new_id].append(raw_electrode_names[k])
     return electrode_names
+
+class RawPotential:
+    @classmethod
+    def import_v0(cls, filename):
+        self = cls()
+        with open(filename, mode="rb") as fh:
+            fh.read(4) # discard
+            self.electrodes = struct.unpack('<I', fh.read(4))[0]
+            self.nx = struct.unpack('<I', fh.read(4))[0]
+            self.ny = struct.unpack('<I', fh.read(4))[0]
+            self.nz = struct.unpack('<I', fh.read(4))[0]
+            vsets = struct.unpack('<I', fh.read(4))[0]
+            # Use mm instead of m
+            self.stride = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # Use mm instead of m
+            self.origin = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # I have no idea what's stored in these
+            fh.read(4)
+            fh.read(4)
+            self.electrodemapping = np.fromfile(fh, np.dtype('<I'), self.electrodes)
+            data = np.fromfile(fh, np.dtype('<d'))
+            if len(data) != self.electrodes * self.nx * self.ny * self.nz:
+                raise ValueError("Did not find the right number of samples")
+            self.data = np.reshape(data, (self.electrodes, self.nx, self.ny, self.nz))
+        return self
+
+    @classmethod
+    def import_v1(cls, filename):
+        self = cls()
+        with open(filename, mode="rb") as fh:
+            fh.read(4) # discard
+            self.electrodes = struct.unpack('<I', fh.read(4))[0]
+            self.nx = struct.unpack('<I', fh.read(4))[0]
+            self.ny = struct.unpack('<I', fh.read(4))[0]
+            self.nz = struct.unpack('<I', fh.read(4))[0]
+            vsets = struct.unpack('<I', fh.read(4))[0]
+            xaxis = (1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0])
+            yaxis = (1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0])
+            # Use mm instead of m
+            self.stride = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # Use mm instead of m
+            self.origin = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # I have no idea what's stored in these
+            fh.read(4)
+            fh.read(4)
+            self.electrodemapping = np.fromfile(fh, np.dtype('<I'), self.electrodes)
+            data = np.fromfile(fh, np.dtype('<d'))
+            if len(data) != self.electrodes * self.nx * self.ny * self.nz:
+                raise ValueError("Did not find the right number of samples")
+            self.data = np.reshape(data, (self.electrodes, self.nx, self.ny, self.nz))
+        return self
+
+    @classmethod
+    def import_64(cls, filename):
+        self = cls()
+        with open(filename, mode="rb") as fh:
+            fh.read(8) # discard
+            self.electrodes = struct.unpack('<Q', fh.read(8))[0]
+            self.nx = struct.unpack('<Q', fh.read(8))[0]
+            self.ny = struct.unpack('<Q', fh.read(8))[0]
+            self.nz = struct.unpack('<Q', fh.read(8))[0]
+            vsets = struct.unpack('<Q', fh.read(8))[0]
+            xaxis = (1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0])
+            yaxis = (1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0],
+                     1000 * struct.unpack('<d', fh.read(8))[0])
+            # Use mm instead of m
+            self.stride = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # Use mm instead of m
+            self.origin = (1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0],
+                           1000 * struct.unpack('<d', fh.read(8))[0])
+            # I have no idea what's stored in these
+            fh.read(8)
+            fh.read(8)
+            electrodemapping = np.fromfile(fh, np.dtype('<Q'), self.electrodes)
+            data = np.fromfile(fh, np.dtype('<d'))
+            if len(data) != self.electrodes * self.nx * self.ny * self.nz:
+                raise ValueError("Did not find the right number of samples")
+            self.data = np.reshape(data, (self.electrodes, self.nx, self.ny, self.nz))
+        return self
